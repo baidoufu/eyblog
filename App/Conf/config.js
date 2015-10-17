d517fe9971681489e099539cc5e7b5bb054f4e31
eyblog <admin@eyblog.com>
2015-10-17 11:12:07 +0800

    加入图片异步上传

--- App/Conf/config.js
+++ App/Conf/config.js
@@ -8,4 +8,8 @@ module.exports = {
   db_user: 'root', // 用户名
   db_pwd: '123456', // 密码
   db_prefix: 'ey_', // 数据库表前缀
+  post_max_file_size: 1024 * 1024 * 1024, // 上传文件大小限制，默认 1G
+  post_max_fields: 100, // 最大表单数，默认为 100
+  post_max_fields_size: 2 * 1024 * 1024, // 单个表单长度最大值，默认为 2MB
+  post_file_upload_path: APP_PATH + '/Runtime/Temp', // 文件上传的临时目录 
 };
